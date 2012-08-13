136a2ec2f7388397e1edaefb60dca6d2f3a71ee1
Tsung W <tsung.bz@gmail.com>
2012-08-13 16:45:39 +0800

    show locations and user of commited comment

--- Journey/Models/PFMUser.m
+++ Journey/Models/PFMUser.m
@@ -146,19 +146,17 @@
     
     [request addBasicAuthenticationHeaderWithUsername:self.email andPassword:self.password];
     
+    NSDictionary* location = [NSDictionary dictionaryWithObjectsAndKeys:@"26.093885", @"lat", @"119.30904", @"lng", nil];
     //NSDictionary* location = [NSDictionary dictionaryWithObjectsAndKeys:@"37.418648", @"lat", @"-122.03125", @"lng", nil];
-    NSDictionary* post = [NSDictionary dictionaryWithObjectsAndKeys:mid, @"moment_id", [comment stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding], @"body", nil];//location, @"location", nil];
-
+    NSDictionary* post = [NSDictionary dictionaryWithObjectsAndKeys:mid, @"moment_id", [comment stringByReplacingPercentEscapesUsingEncoding:NSUTF8StringEncoding], @"body", //nil];
+        location, @"location", nil];
     
     [request setPostFormat:ASIMultipartFormDataPostFormat];
     [request setPostValue:[post JSONRepresentation] forKey:@"post"];
-    // [request setPostValue:@"{lat:26.093885,lng:119.30904" forKey:@"location"];
     
     [request setCompletionBlock:^{
         if(request.responseStatusCode == 200) {
             [self parseCommentsJSON:[request responseString]];
-            
-            //[self.momentsDelegate didFetchComments:[request responseString]];
         } else {
             //[self.momentsDelegate didFailToFetchMoments];
         }
@@ -168,7 +166,6 @@
         //[self.momentsDelegate didFailToFetchMoments];
     }];
     
-    //[self parseCommentsJSON:@"{\"comments\": {\"502636cf06727b26630224df\": [{\"body\": \"\u96e8\u540e\u5c0f\u6eaa\", \"created\": 1344681588.182, \"id\": \"502636cf06727b26630224e0\", \"location_id\": \"502636d78319cd04941ad611\", \"moment_id\": \"502636cf06727b26630224df\", \"state\": \"live\", \"user_id\": \"501a66e2fa63b3744c000a31\"}, {\"body\": \"\u4f60\u5bb6\u9644\u8fd1\u5417\uff1f\", \"created\": 1344826240.369075, \"id\": \"50286b8021c7ea54f4049311\", \"moment_id\": \"502636cf06727b26630224df\", \"state\": \"live\", \"user_id\": \"4db7b2e46e5e6a75790000db\"}]}, \"emotions\": {}, \"locations\": {\"502636d78319cd04941ad611\": {\"created\": 1344681588.182, \"id\": \"502636d78319cd04941ad611\", \"lat\": 24.94327, \"lng\": 118.31857, \"location\": {\"administrative_area_level_1\": \"Fujian\", \"city\": \"Quanzhou\", \"city_id\": \"4ec319876a9eba1db6000025\", \"country\": \"CN\", \"country_name\": \"China\", \"gc\": \"g\", \"lat\": 24.94327, \"lng\": 118.31857, \"lnglat\": [118.31857, 24.94327], \"province\": \"Fujian\", \"province_name\": \"Fujian\", \"sublocality\": \"Nan'an\"}, \"user_id\": \"501a66e2fa63b3744c000a31\"}}, \"nudges\": {\"502636cf06727b26630224df\": []}, \"seen_it_totals\": {\"502636cf06727b26630224df\": 2}, \"users\": {\"4db7b2e46e5e6a75790000db\": {\"created_at\": \"2011-04-27 06:08:37\", \"first_name\": \"Tsung\", \"gender\": \"male\", \"id\": \"4db7b2e46e5e6a75790000db\", \"last_name\": \"Wu\", \"photo\": {\"ios\": {\"1x\": {\"file\": \"processed_80x80.jpg\", \"height\": 80, \"width\": 80}, \"2x\": {\"file\": \"processed_160x160.jpg\", \"height\": 160, \"width\": 160}}, \"original\": {\"file\": \"original.jpg\", \"height\": 160, \"width\": 160}, \"url\": \"https://s3-us-west-1.amazonaws.com/images.path.com/profile_photos/0570d1783b550ccbaee0739c58cca81e46ab57c6\", \"version\": 3}, \"primary_network\": \"path\", \"state\": \"enabled\", \"username\": \"ioNull\"}, \"501a66e2fa63b3744c000a31\": {\"created_at\": \"2012-08-02 11:39:14\", \"first_name\": \"congwei\", \"gender\": \"male\", \"id\": \"501a66e2fa63b3744c000a31\", \"last_name\": \"hong\", \"primary_network\": \"path\", \"state\": \"enabled\", \"username\": \"congwei-hong\"}}}"];
     [request startAsynchronous];
     return request;
 }
@@ -240,6 +237,30 @@
 
 - (void)parseCommentsJSON:(NSString *)json {
     NSMutableDictionary *dict = [json JSONValue];
+    
+    // Get the locations map
+    NSDictionary * locationsDict = (NSDictionary *)[dict objectOrNilForKey:@"locations"];
+    
+    [locationsDict enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
+        PFMLocation * location = [PFMLocation locationFrom:(NSDictionary *)obj];
+        [[NSApp sharedLocations] setObject:location forKey:key];
+    }];
+    
+    // Get the global users map
+    NSDictionary * usersDict = (NSDictionary *)[dict objectOrNilForKey:@"users"];
+    [usersDict enumerateKeysAndObjectsUsingBlock:^(id key, id obj, BOOL *stop) {
+        NSDictionary * userDict = (NSDictionary *)obj;
+        NSString * userId = (NSString *)key;
+        
+        PFMUser * user = [PFMUser new];
+        user.oid = userId;
+        user.firstName    = [userDict objectOrNilForKey:@"first_name"];
+        user.lastName     = [userDict objectOrNilForKey:@"last_name"];
+        user.profilePhoto = [PFMPhoto photoFrom:[userDict objectOrNilForKey:@"photo"]];
+        
+        [[NSApp sharedUsers] setObject:user forKey:userId];
+    }];
+    
     NSMutableDictionary* comments = (NSMutableDictionary *)[dict objectOrNilForKey:@"comments"];
     for(NSString* key in comments) {
         NSMutableArray* commentsDict = (NSMutableArray*)[comments objectForKey:key];
